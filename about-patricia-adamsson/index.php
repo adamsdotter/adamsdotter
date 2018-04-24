c857e7927cd4b91242f958a17e109d147b9e185c
Patricia Adamsson <patricia.adamsson@gmail.com>
2018-04-24 23:20:19 +0200

    Some files

---
+++ about-patricia-adamsson/index.php
@@ -0,0 +1,36 @@
+<?php
+
+	$title = 'About Patricia Adamsson, a.k.a Mjau';
+	$description = 'Get to know Web Developer Patricia Adamsson.';
+	$canonical = 'http://www.adamsdotter.se/about-patricia-adamsson/';
+	$pageName = 'mjau about';
+
+	include_once('../assets/includes/head.php');
+	include_once('../assets/includes/header.php');
+?>
+		<article role="main">
+			<section>
+				<h1>About <span>Patricia Adamsson</span></h1>
+				<div>
+					<p>
+						My name is Patricia. I am a Frontend Developer.
+						Creating websites has been my biggest interest since the age of 13. In 1999 I built my first website using cool frames, table layouts,
+						java applets and the amazing <code>&lt;marquee&gt;</code> and <code>&lt;blink&gt;</code> tags. Since then I've been loving to build websites.
+					</p>
+					<p>
+						I've been working in the web industry since 2008, and have since then been loving every day at work - because I get to <i>work</i> with my hobby! &lt;3
+					</p>
+					<p>
+						On my spare time, you'll find me in front of my computer, probably coding on one of my many side projects.
+						When not coding I love to travel, and try to do it as often as I possibly can.
+					</p>
+				</div>
+			</section>
+			<div class="shelf">
+				<div class="poster"></div>
+				<div class="heels"></div>
+				<div class="sega"></div>
+			</div>
+		</article>
+
+<?php include_once('../assets/includes/footer.php'); ?>
---
+++ contact/index.php
@@ -0,0 +1,30 @@
+<?php
+	$title = 'Contact Patricia Adamsson, aka Mjau';
+	$description = 'How to get in contact with Patricia Adamsson.';
+	$canonical = 'http://www.adamsdotter.se/contact/';
+	$pageName = 'mjau contact';
+
+	include_once('../assets/includes/head.php');
+	include_once('../assets/includes/header.php');
+?>
+
+	<article role="main">
+		<section>
+			<h1>Contact <span>Patricia Adamsson</span></h1>
+			<p>Don't hesitate to hit me up if you've got any questions or just wanna drop a line.</p>
+			<dl>
+				<dt>Email:</dt><dd><a href="mailto:mjau@adamsdotter.se">mjau@adamsdotter.se</a></dd>
+				<dt>Twitter:</dt><dd><a href="http://twitter.com/_mjau">_mjau</a></dd>
+				<dt>LinkedIn:</dt><dd><a href="http://www.linkedin.com/in/patriciaadamsson">patriciaadamsson</a></dd>
+				<dt>Github:</dt><dd><a href="https://github.com/adamsdotter">@adamsdotter</a></dd>
+			</dl>
+		</section>
+		<div class="shelf">
+			<div class="sailor-moon"></div>
+			<div class="film"></div>
+			<div class="bamboo"></div>
+			<div class="poster"></div>
+		</div>
+	</article>
+
+<?php include_once('../assets/includes/footer.php'); ?>
