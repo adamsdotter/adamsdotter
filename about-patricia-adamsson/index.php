240d2c3d1a3547988166b39803a01439e4897d8a
Patricia Adamsson <patricia.adamsson@gmail.com>
2019-03-26 22:21:24 +0100

    correct spelling

--- about-patricia-adamsson/index.php
+++ about-patricia-adamsson/index.php
@@ -10,8 +10,7 @@
 ?>
 		<article role="main">
 			<section>
-				<h1>Hi! I'm<span> Patricia Adamsson</span></h1>
-				<h2>I am a freelancing Front-end developer.</h2>
+				<h1>About <span> Patricia Adamsson</span></h1>
 				<div>
 					<p>
 						At the age of 13 I got my first computer and immediately I was hooked.
@@ -22,14 +21,14 @@
 						but the passion for building websites still remained and seem to never be growing old.
 					</p>
 					<p>
-						I’ve been working as a Frontend Developer professionally, full-time since 2008.
+						I’ve been working as a Front-end developer professionally, full-time since 2008.
 						In 2014 I moved to Berlin but continued to be employed in Stockholm.
 						I ended up working over 4 years 100% remotely from Berlin.
 						In January 2018 I became a freelancer. Since then I’ve been working on a Stockholm based project,
 						1-2 weeks in Stockholm and the rest remotely in Berlin.
 					</p>
 					<p>
-						My biggest passion lays within Frontend techniques such as JavaScript, CSS and HTML.
+						My biggest passion lays within Front-end techniques such as JavaScript, CSS and HTML.
 						I love to build semantically correct, responsive websites which are available on all devices and for everyone.
 						Therefore are a11y and SEO two very important things that I always keep in the back of my head when coding.
 						Until 2015 I was mainly jQuery-focused.
